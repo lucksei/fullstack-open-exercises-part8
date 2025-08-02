becd63b68667f02af690ee1a99d96a2aea0569ba
Luca Seimandi <lseimandi@colsecor.com.ar>
2025-08-02 00:00:26 -0300

    allBooks works now, and validation is also done -- exercises 8.14 and 8.15 done

--- library-backend/graphql/resolvers.js
+++ library-backend/graphql/resolvers.js
@@ -2,6 +2,7 @@ const { randomUUID } = require('crypto')
 const Author = require('../schemas/author')
 const Book = require('../schemas/book')
 const { authors, books } = require('../utils/mockData')
+const { GraphQLError } = require('graphql')
 
 
 
@@ -9,50 +10,57 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: (root, args) => {
-      let _books = books
+    allBooks: async (root, args) => {
+      let booksQuery = Book.find({}).populate('author')
       if (args.author) {
-        _books = _books.filter((book) => book.author === args.author)
+        booksQuery = booksQuery.where('author').equals(args.author)
       }
       if (args.genre) {
-        _books = _books.filter((book) => book.genres.includes(args.genre))
+        booksQuery = booksQuery.where('genres').equals(args.genre)
       }
-      return _books
+      let books = await booksQuery
+      return books
     },
     allAuthors: () => authors.map((author) => appendBookCount(author, books)),
   },
   Mutation: {
     addBook: async (root, args) => {
-      console.log("executing")
       let bookAuthor = await Author.findOne({ name: args.author })
-      console.log(bookAuthor)
       if (!bookAuthor) {
         bookAuthor = new Author({
           name: args.author,
           born: null
         })
-        bookAuthor = await bookAuthor.save()
+        try {
+          bookAuthor = await bookAuthor.save()
+        } catch (error) {
+          throw new GraphQLError("Error saving author", {
+            extensions: {
+              code: "BAD_USER_INPUT",
+              invalidArgs: args,
+              error,
+            }
+          })
+        }
       }
-      console.log(bookAuthor)
       const newBook = new Book({
         title: args.title,
         published: args.published,
         author: bookAuthor._id,
         genres: args.genres
       })
-      const savedBook = await newBook.save()
-      return savedBook.populate('author')
-      // const author = authors.find((author) => author.name === args.author)
-      // if (!author) {
-      //   const newAuthor = {
-      //     name: args.author,
-      //     born: null
-      //   }
-      //   authors = authors.concat(newAuthor)
-      // }
-      // const book = { ...args, id: randomUUID() }
-      // books = books.concat(book)
-      // return book
+      try {
+        const savedBook = await newBook.save()
+        return savedBook.populate('author')
+      } catch (error) {
+        throw new GraphQLError("Error saving book", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: args,
+            error,
+          }
+        })
+      }
     },
     editAuthor: (root, args) => {
       const author = authors.find((author) => author.name === args.name)
